cfde7bb9fe8e2973948981df86853dec5f6b3ba4
Trouss <47436624+Reenzoo@users.noreply.github.com>
2019-02-08 20:51:18 -0300

    2do dia de programacion

---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,34 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+mostrar el resulto por medio de "ALERT"*/
+function sumar()
+{
+
+       var primerNumero;
+       var segundoNumero;
+       var sumar;
+
+       primerNumero= numeroUno.value;
+       segundoNumero= numeroDos.value;
+
+       primerNumero= parseInt(primerNumero);
+       segundoNumero= parseInt(segundoNumero);
+
+
+       sumar=primerNumero + segundoNumero;
+
+       alert("el resultado es: " +sumar); 
+
+
+
+
+
+
+}
+
+// hay 10 tipos de persona *2 personas* codigo binario 
+//parseInt= toma dato y lo trasforma  
+
+
+
+// 
