82af43fa096b5da5982ce865253881c2dab6aa52
Trouss <47436624+Reenzoo@users.noreply.github.com>
2019-02-08 22:04:22 -0300

    ejercicio 8
    detectar errores

--- jsEntradaSalida-8.js
+++ jsEntradaSalida-8.js
@@ -4,5 +4,22 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var dividendo;
+	var divisor;
+	var SacarResto;
+
+    dividendo= dividendo.value;
+	divisor= divisor.value;
+
+	dividendo= document.getElementById('dividendo').value;
+	divisor= document.getElementById('divisor').value;
+
+    dividendo= parseInt(dividendo);
+    divisor= parseInt(divisor);
+
+    SacarResto= dividendo % divisor;
+
+    alert("El resto es: " % SacarResto);
+
+
 }
