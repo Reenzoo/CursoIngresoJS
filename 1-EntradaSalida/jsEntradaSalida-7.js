926709d7db0b79de745dae2849d5cef5fa9c647f
Trouss <47436624+Reenzoo@users.noreply.github.com>
2019-02-14 17:25:47 -0300

    Tarea en casa
    tarea en casa punto 7 y 9

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,70 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	   var primerNumero;
+       var segundoNumero;
+       var sumar;
+
+       primerNumero= numeroUno.value;
+       segundoNumero= numeroDos.value;
+
+       primerNumero= parseInt(primerNumero);
+       segundoNumero= parseInt(segundoNumero);
+
+
+       sumar=primerNumero + segundoNumero;
+
+       alert("el resultado es: "+sumar);
 }
 
 function restar()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var resta;
+
+	primerNumero= numeroUno.value;
+	segundoNumero= numeroDos.value;
+
+	primerNumero= parseInt(primerNumero);
+	segundoNumero= parseInt(segundoNumero);
+
+	resta= primerNumero - segundoNumero;
+
+	alert("el reultado es: "+resta);
 }
 
 function multiplicar()
 { 
-	
+	var primerNumero;
+	var segundoNumero;
+	var multiplicar;
+
+	primerNumero= numeroUno.value;
+	segundoNumero= numeroDos.value;
+
+	primerNumero= parseInt(primerNumero);
+	segundoNumero= parseInt(segundoNumero);
+
+	multiplicar= primerNumero * segundoNumero;
+
+	alert("El resultado es: "+multiplicar);
 }
 
 function dividir()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var dividir;
+
+	primerNumero=numeroUno.value;
+	segundoNumero=numeroDos.value;
+
+	primerNumero= parseInt(primerNumero);
+	segundoNumero= parseInt(segundoNumero);
+
+	dividir= primerNumero / segundoNumero;
+
+	alert ("El resultado es: "+dividir);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,22 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
+	var primerNumero;
+	var total;
+	var mostrarAumento
 	
+
+	primerNumero= sueldo.value;
+	total= resultado.value;
+
+	primerNumero=document.getElementById('sueldo').value;
+	total=document.getElementById('resultado').value;
+
+	primerNumero= parseInt(primerNumero);
+    total= parseInt(total);
+
+    mostrarAumento= ((10*primerNumero)/100);
+	
+	RESULTADO(primerNumero+mostrarAumento);
+ 
 }
