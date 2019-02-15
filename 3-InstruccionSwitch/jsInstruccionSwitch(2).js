a3544efd0bdc5dd4cec11452c3120f705d5b9465
alumno <alumno>
2019-02-15 20:47:11 -0300

    Ejercicio de switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -2,10 +2,35 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
+var mensaje;
 alert (mesDelAño);
 
 
+switch (mesDelAño)
+ {
+    case "Julio":
+
+    case "Agosto":
+         mensaje="Abrigate que hace frio";
+         break;
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre":
+    case "Diciembre":
+         mensaje="Ya pasamos el frio";
+         break;
+    case "Enero":
+    case "Febrero":
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Julio":
+          mensaje= "Falta para el invierno"
+          break;
+
+}
+
+alert(mensaje);
 
 
 }//FIN DE LA FUNCIÓN
