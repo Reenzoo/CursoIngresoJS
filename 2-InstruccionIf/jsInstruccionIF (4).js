1a88bc05eac75cc63baa9cffd57364db38d3e279
Trouss <47436624+Reenzoo@users.noreply.github.com>
2019-02-15 16:17:35 -0300

    If ejercicios
    Ejercicios 4 y 5

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,19 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad;
+    var mensaje;
+    
+    edad= document.getElementById('edad').value;
+    edad= parseInt(edad);
+    if(edad<18)
+    {
+    	if(edad>12)
+    	{
+    		mensaje="Es adolecente";
+    	}
+    }
 
-
+    alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,21 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad;
+    var mensaje;
+    
+    edad= document.getElementById('edad').value;
+    edad= parseInt(edad);
+   
+    if (edad>=18) 
+            {
+               if (edad<=12)
+               {
+               	mensaje="No es adolecente";
+               }
+            	
+            }
 
+    alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
