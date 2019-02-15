9066fe76f3f9b6a7e84ba20f0324204df2cc08c0
Trouss <47436624+Reenzoo@users.noreply.github.com>
2019-02-14 21:23:55 -0300

    Tps
    Dia 14 de Febrero

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,13 @@
 function mostrar()
 {
+  var compra;
+  var descuento;
+  var iva;
+
+  compra= prompt("Tu compra es de "," 5000" );
+  descuento= prompt("Y tenes 10% de descuento, que seria:",compra-(compra*10/100));
+  iva= prompt("Pero inclueyendo el iva, te quedaria final: ",descuento+(descuento*5/100))
   
+
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,30 @@
 
 function mostrar()
 {
+   
+     var mascota1;
+     var mascota2;
+     var peso1;
+     var peso2;
+     var sumar;
+
+     mascota1=prompt("¿Como se llama?","India");
+     mascota2=prompt("¿Como se llama?","Pelufo");
+     
+     peso1=prompt("¿Cuanto pesa?","50");
+     peso2=prompt("¿Cuanto pesa?","40");
+
+     peso1=parseInt(peso1);
+     peso2=parseInt(peso2);
+
+
+     sumar=peso1 + peso2;
+
+     alert("Se llaman "+mascota1+" y "+mascota2 +" y los dos juntos pesan: "+sumar +" kilos ");
+
+
+
+
+
 
 }
